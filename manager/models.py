5a48b4703288e07333b91936d20758305e4dd769
Tymur Semelinskyi <t.semelinsky@gmail.com>
2023-08-13 18:09:15 +0200

    added Position model with name field, ordering by name and overriden __str__ class

--- manager/models.py
+++ manager/models.py
@@ -34,3 +34,13 @@ class Task(models.Model):
         on_delete=models.CASCADE,
         related_name="task_types"
     )
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
